03e25ac3dd8942c59ee366339e7e2454050aa04e
luc siat <lucsiat@gmail.com>
2022-11-29 17:15:20 +0100

    recreating the github css
    Signed-off-by: luc siat <lucsiat@gmail.com>

--- dotnet-trainingGround2/TrainingGround/IPrintable.cs
+++
@@ -1,4 +0,0 @@
-public interface IPrintable
-{
-  public string GetPrintString();
-}
---
+++ training-ground-main-with-xunit/mvcDotNetTrainingGround.Tests/DevelopersControllerTests.cs
@@ -0,0 +1,85 @@
+using System;
+using Xunit;
+namespace mvcDotNetTrainingGround.Tests;
+
+public class DevelopersControllerTests
+{
+[Fact]
+  public void checking_index()
+  {
+    // arrange
+    var controller = new DevelopersController();
+    // act
+    var result = controller.Index();
+    // assert
+    var redirectToActionResult =
+      Assert.IsType<redirectToActionResult>(result);
+    Assert.Equal("Index", redirectToActionResult.ActionName);
+  }
+//   [Fact]
+//   public void checking_create()
+//   {
+//     // arrange
+//     var controller = new DevelopersController();
+//     // act
+//     var result =  controller.Details(1);
+//     // assert
+//     var redirectToActionResult =
+//       Assert.IsType<redirectToActionResult>(result);
+//     Assert.Equal("Details", redirectToActionResult.ActionName);
+//   }
+// [Fact]
+//   public void checking_details()
+//   {
+//     var controller = new DevelopersController();
+//     // act
+//     var result = controller.Create();
+//     // assert
+//     var redirectToActionResult =
+//       Assert.IsType<redirectToActionResult>(result);
+//     Assert.Equal("Create", redirectToActionResult.ActionName);
+//   }
+}
+
+// [Fact]
+// public async Task IndexPost_ReturnsBadRequestResult_WhenModelStateIsInvalid()
+// {
+//     // Arrange
+//     var mockRepo = new Mock<IBrainstormSessionRepository>();
+//     mockRepo.Setup(repo => repo.ListAsync())
+//         .ReturnsAsync(GetTestSessions());
+//     var controller = new HomeController(mockRepo.Object);
+//     controller.ModelState.AddModelError("SessionName", "Required");
+//     var newSession = new HomeController.NewSessionModel();
+
+//     // Act
+//     var result = await controller.Index(newSession);
+
+//     // Assert
+//     var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+//     Assert.IsType<SerializableError>(badRequestResult.Value);
+// }
+
+// [Fact]
+// public async Task IndexPost_ReturnsARedirectAndAddsSession_WhenModelStateIsValid()
+// {
+//     // Arrange
+//     var mockRepo = new Mock<IBrainstormSessionRepository>();
+//     mockRepo.Setup(repo => repo.AddAsync(It.IsAny<BrainstormSession>()))
+//         .Returns(Task.CompletedTask)
+//         .Verifiable();
+//     var controller = new HomeController(mockRepo.Object);
+//     var newSession = new HomeController.NewSessionModel()
+//     {
+//         SessionName = "Test Name"
+//     };
+
+//     // Act
+//     var result = await controller.Index(newSession);
+
+//     // Assert
+//     var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+//     Assert.Null(redirectToActionResult.ControllerName);
+//     Assert.Equal("Index", redirectToActionResult.ActionName);
+//     mockRepo.Verify();
+// }
