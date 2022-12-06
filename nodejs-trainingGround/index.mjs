43e20d74390830b9bb67973ed2f80f7b17e3e145
luc siat <lucsiat@gmail.com>
2022-12-06 15:20:54 +0100

    variables passed
    Signed-off-by: luc siat <lucsiat@gmail.com>

--- nodejs-trainingGround/index.mjs
+++ nodejs-trainingGround/index.mjs
@@ -1,1 +1,16 @@
 export const getAge = (birthYear, currentYear) => currentYear - birthYear;
+
+export const getAgeGroup = (age) => {
+  if(age === 50)
+   return 'prime';
+  if(age > 39)
+    return 'you are just old';
+  if(age > 19)
+    return 'adult';
+  if(age > 12)
+    return 'teenager';
+  if(age >= 4)
+    return 'kid';
+  if(age >= 0)
+    return 'toddler';
+}
--- nodejs-trainingGround/tests.mjs
+++ nodejs-trainingGround/tests.mjs
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { getAge } from './index.mjs'
+import { getAge, getAgeGroup } from './index.mjs';
 
 describe('age calculator', () => {
   it('someone born 1972 is 50 2022', () => {
@@ -20,5 +20,63 @@ describe('age calculator', () => {
     const result = getAge(1995, 2022);
     // assert
     assert.equal(result, 27);
+  })
+});
+
+describe('age classifier', () => {
+  it('0-3 should be toddler', () => {
+  // arrange
+  // act
+  const result = getAgeGroup(0);
+  // assert
+  assert.equal(result, 'toddler');
+  })
+  it('4-12 years old should be a kid', () => {
+    // act
+    const result = getAgeGroup(12);
+    // assert
+    assert.equal(result, 'kid');
+  })
+  it('13-19 years old should be a teenager', () => {
+    // act
+    const result = getAgeGroup(13);
+    // assert
+    assert.equal(result, 'teenager');
+  })
+  it('20-39 years old should be a adult', () => {
+    // act
+    const result = getAgeGroup(24);
+    // assert
+    assert.equal(result, 'adult');
+  })
+  it('above 39 years old ... you are just old', () => {
+    // act
+    const result = getAgeGroup(40);
+    // assert
+    assert.equal(result, 'you are just old');
+  })
+})
+
+describe('constant and variables', () => {
+  it.skip('constant means constant', () => {
+    const aNumber = 1;
+    aNumber = 2;
+  });
+  it('you can change a let variable', () => {
+    // arrange
+    let aNumber = 1;
+
+    // act
+    aNumber = 2;
+
+    // assert
+    assert.equal(aNumber, 2);
+  });
+  it('const is undefiner', () => {
+    // arrange
+    let aNumber;
+    // assert
+    assert.equal('undefined', typeof aNumber);
+    assert.equal(undefined, aNumber);
   })
 });
